429c4bf1e781b715fec52415b4b17f6f7571cb8f
iqra abdul rauf <iqraabdulrauf01@gmail.com>
2019-08-15 22:09:51 +0500

    adding portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://iqraabdulrauf.github.io/",
+    "name": "Iqra Abdul Rauf",
+    "role": "Full stack Blockchain Developer",
+    "linkedinId":" https://linkedin.com/in/iqra-abdul-rauf-327034114/",
+    "skypeid": "iqraabdulrauf01@outlook.com",
+    "roleDescription": `I am computer science graduate and working in software development
+    industry as a Full stack blockchain developer.I am tech savy and always willing to 
+    learn new technologies to enhance my skills and grow professionally.`,
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/iqra-abdul-rauf-327034114/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/iqraabdulrauf",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.facebook.com/iqra.rauf.71",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":`I am a tech enthusiast, computer science graduate and  and working in software development
+    industry as a Full stack blockchain developer.I am tech savy and always willing to 
+    learn new technologies to enhance my skills and grow professionally.`,
+    "address":"Karachi,Pakistan",
+    "website":"https://iqraabdulrauf.github.io",
+    "education":[
+      {
+        "UniversityName":"NED University Of Engineering And Technology",
+        "specialization":"Computer Science & Information Technology",
+        "PassingDate":"Sept-2018",
+        "Achievements":"Silver medalist | JICE-JENESYS Participant 2017"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Avanza Innovations LLC",
+        "specialization":"Blockchain Developer",
+        "JoiningDate":"Sept-2018"
+      },
+      {
+        "CompanyName":"CloudChef",
+        "specialization":"Internee Android Developer",
+        "JoiningDate":"Oct-2017"
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Golang"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Blockchain"
+      },
+      {
+        "skillname":"Ethereum | Hyperledger"
+      },
+      {
+        "skillname":"Smart Contracts"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"If you are going through hell,go through it.Be unstoppable!",
+        "name":""
+      },
+      {
+        "description":"There is no talent,only HARD WORK !",
+        "name":""
+      }
+      
+    ]
+  }
+  
+  export default resumeData
